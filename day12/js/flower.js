fc71ef04869034f9dd27cb44bd63f5518fb06ad5
alsdn82 <vhrcnsehr@naver.com>
2021-08-27 13:18:48 +0900

    day13 소스파일

---
+++ day12/js/flower.js
@@ -0,0 +1,8 @@
+function showText(){
+    document.getElementById("desc").style.display = "block";
+    document.getElementById("open").style.display = "none";
+}
+function hideText(){
+    document.getElementById("desc").style.display = "none";
+    document.getElementById("open").style.display = "block";
+}
